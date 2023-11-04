16cc45c6b39367986069d2bb6625262646c0d340
Ahmet Kürşad YILDIZ <ahmetkursatyildiz_53@hotmail.com>
2023-11-05 00:11:22 +0300

    @model öğrenildi controller view model arasında veri aktarıldı

--- temeller/Controllers/EmployeeController.cs
+++ temeller/Controllers/EmployeeController.cs
@@ -1,19 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
+using temeller.Models;
 
 namespace Basics.Controllers
 {
     public class EmployeeController : Controller
     {
         public IActionResult Index1(){
-            return View();
+            string message = $"Hello World {DateTime.Now.ToString()}";
+            return View("Index1",message);
         }
 
-        public ViewResult Index2(){
-            return View("Index");
+        public ViewResult Index2()
+        {
+            var names = new String[]{
+                "Ahmet",
+                "Mehmet",
+                "Can"
+            };
+            return View(names);
         }
 
         public IActionResult Index3(){
-            return Content("Employee");
+
+            var list = new List<Employee>(){
+                new Employee(){Id=1,FirstName="Ahmet",LastName="Can",Age=20},
+                new Employee(){Id=2,FirstName="Can",LastName="Dağ",Age=25},
+                new Employee(){Id=3,FirstName="Demir",LastName="Güneş",Age=37}
+            };
+            return View("Index3",list);
         }
     }
 
